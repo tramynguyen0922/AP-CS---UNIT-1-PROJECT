1fe841352a3ed7946e1e7b966690ed4f524b48a8
tramynguyen0922 <69646105+tramynguyen0922@users.noreply.github.com>
2020-09-07 14:51:48 -0700

    Update and rename Code to HeatEnergyRunner.java

--- HeatEnergyRunner.java
+++ HeatEnergyRunner.java
@@ -5,16 +5,12 @@ Scanner reader = new Scanner(System.in);
 
 System.out.println("Enter the mass of the water, in grams");
 double mass = reader.nextDouble();
-System.out.println("Enter the intial temperature of the water, in
-
-Celsius");
+System.out.println("Enter the intial temperature of the water, in Celsius");
 
 double initialTemp = reader.nextDouble();
 if(initialTemp < -273.14)
 initialTemp = -273.14;
-System.out.println("Enter the final temperature of the water, in
-
-Celsius");
+System.out.println("Enter the final temperature of the water, in Celsius");
 
 double finalTemp = reader.nextDouble();
 if(finalTemp < -273.14)
@@ -68,6 +64,7 @@ else
 System.out.println("Cooling (liquid): " + energyChange + "kJ");
 return energyChange;
 }
+
 public static double tempChangeVapor(double mass, double startTemp, double
 endTemp, String endPhase, boolean endothermic) {
 if(!endPhase.equals("vapor"))
@@ -79,6 +76,7 @@ else
 System.out.println("Cooling (vapor): " + energyChange + "kJ");
 return energyChange;
 }
+
 public static double fusion(double mass, boolean endothermic) {
 double energyChange;
 if(endothermic) {
